395fe69c0a91ff6fcd01acadc8c2bf6380cd4124
Kyle <TheRealBanana@users.noreply.github.com>
2018-07-26 17:09:09 -0700

    Resolution update
    lowered the resolution to make it faster. Figuring out how I am going to do zooming/panning.

--- src/mandelbrot.kt
+++ src/mandelbrot.kt
@@ -19,11 +19,11 @@ const val WINDOW_SIZE_HEIGHT = 900
 var window: Long = NULL
 
 //WORRY ABOUT RESOLUTION (diff per iteration x and y)
-const val RESOLUTION_LIMIT_Y: Double = 0.001
-const val RESOLUTION_LIMIT_X: Double = 0.001
+const val RESOLUTION_LIMIT_Y: Double = 0.01
+const val RESOLUTION_LIMIT_X: Double = 0.01
 
 //How many iterations should we run before we are certain of an escape velocity?
-const val ESCAPE_VELOCITY_TEST_ITERATIONS: Int = 300
+const val ESCAPE_VELOCITY_TEST_ITERATIONS: Int = 100
 
 data class Color(val r: Double, val g: Double, val b: Double)
 
@@ -61,7 +61,7 @@ fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HE
     // GL configuration comes AFTER we make the window our current context, otherwise errors
     GL.createCapabilities()
     GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f) //white background
-    GL11.glOrtho(-2.2, 1.2, -1.2, 1.2, -1.0, 1.0)
+    GL11.glOrtho(-2.0, 1.0, -1.0, 1.0, -1.0, 1.0)
     GL11.glViewport(0, 0, WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT)
     glfwShowWindow(window)
 }
@@ -70,25 +70,8 @@ fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HE
 //If it escapes (is greater than 2) it returns the velocity.
 //If its bounded it returns 0
 fun findEscapeVelocity(c: ComplexNumber): Double {
-
-    var v: Double = 0.0
     var z: ComplexNumber = ComplexNumber(0.0, 0.0)
     for (iter in 1..ESCAPE_VELOCITY_TEST_ITERATIONS) {
-        v += z.magnitude()
-        if (z.magnitude() > 2.0) {
-            return v / iter
-        }
-        z = z*z + c
-    }
-    return 0.0
-}
-
-fun findEscapeVelocity2(c: ComplexNumber): Double {
-
-    var v: Double = 0.0
-    var z: ComplexNumber = ComplexNumber(0.0, 0.0)
-    for (iter in 1..ESCAPE_VELOCITY_TEST_ITERATIONS) {
-        v += z.magnitude()
         if (z.magnitude() > 2.0) {
             return iter.toDouble()
         }
@@ -111,14 +94,12 @@ fun mandelbrotsimple(): Map<ComplexNumber, Color> {
         var real: Double = -2.0
         while (real in -2.0..1.0) {
             //doing stuffs, mandelbrot-ey stuff
-            val escapevelocity: Double = findEscapeVelocity2(ComplexNumber(real, curImagCoord))
+            val escapevelocity: Double = findEscapeVelocity(ComplexNumber(real, curImagCoord))
             if (escapevelocity == 0.0) {
                 cordlist[ComplexNumber(real, curImagCoord)] = Color(0.0,0.0,0.0) //Black color, inside the set
             } //in the set, bounded value
             else {
-                //println("Velocity for point (${real}, ${curImagCoord}) is $escapevelocity")
                 cordlist[ComplexNumber(real, curImagCoord)] = Color(escapevelocity/ESCAPE_VELOCITY_TEST_ITERATIONS,0.0,0.0) //Black color, inside the set
-                //Non-bounded value. Color by its velocity. Meh will do later
             }
             real += RESOLUTION_LIMIT_X
         }
@@ -136,7 +117,7 @@ fun main(args: Array<String>) {
     println("Done generating set, printing to window...")
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents()
-        glPointSize(1.0f)
+        glPointSize(5.0f)
         glClear(GL_COLOR_BUFFER_BIT)
         glBegin(GL_POINTS)
         for ((cord, color) in setcoords) {
