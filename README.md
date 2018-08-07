c3653abe94725d7e3593b2065edbfec91d47f769
Kyle <TheRealBanana@users.noreply.github.com>
2018-08-07 15:52:51 -0700

    Pan Working, Zoom sortof.
    Pan/zoom is basically implemented. I thought getting the zooming in would be easy but converting from on-screen coodinates to my coordinates in my ortho projection isn't that easy with LWJGL3 since it doesn't include gluUnProject(). Will figure out another way to do it, I think I already have some idea.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ More boredom means more coding. Decided to do what everyone and their grandma's 
 This code is super ugly because I wanted to make it without looking at any other code, just me and the maths. 
 In paricular I'm not quite sure how best to color based on escape velocities (or even how to calculate the escape velocity in a sane way)
 
-At least it prints something though so I'm happy. Multithreading will also need to come since this is super slow.
+At least it prints something though so I'm happy. Recoding using GLSL will be necessary for anything approaching realtime performance.
 
 
 Found an interesting coloring scheme using sine and cosine functions with the escape velocity (both normalized to the max number of iterations and unnormalized) shown below. Will be interesting to see how the color evolves as we get pan/zoom implemented.
-![Screenshot](https://i.imgur.com/u3TwINn.jpg)
+![Screenshot](https://i.imgur.com/VwPGoYX.png)
--- src/mandelbrot.kt
+++ src/mandelbrot.kt
@@ -9,42 +9,30 @@ import kotlin.math.sin
 import kotlin.math.cos
 import kotlin.math.sqrt
 
-//auto calculate point size by looking at resolution limit?
-// distance between two points separated by resolution limit
-// have to calculate the actual display coordinates from the point locations
-
-//OK So we want to know how many pixels span one tick of the RESOLUTION_LIMIT_* values
-//That should
-
-//movement amount should be percentage of screen width
-
-//Ok so the way we are going to handle zooming/panning is by setting an origin and bound
-//Zoomed all the way out is x=-2, y=1
-var CURRENT_X_LIM: Double = -2.0
-var CURRENT_Y_LIM: Double = 1.0
-var ORIGIN_X: Double = CURRENT_X_LIM
-var ORIGIN_Y: Double = CURRENT_Y_LIM
-
+//Zoom/pan increment stuffs
+const val ZOOM_INCREMENT = 0.3 //increase by 0.3
+const val PAN_PCT_INCREMENT = 0.15 //Move view by 15%
 
 //GL STUFFS
-const val WINDOW_SIZE_WIDTH = 1280
-const val WINDOW_SIZE_HEIGHT = 900
+const val WINDOW_SIZE_WIDTH = 1680
+const val WINDOW_SIZE_HEIGHT = 1050
 
 var window: Long = NULL
 
 //WORRY ABOUT RESOLUTION (diff per iteration x and y)
+//This will go away completely with zooming since we need to dynamically
+//set our resolution and pixel size limits.
+//0.01 with a width of 3 results in 300 iterations.
+//Our width (1680) divided by 300 = our pixel point size (5.6)
 const val RESOLUTION_LIMIT_Y: Double = 0.01
 const val RESOLUTION_LIMIT_X: Double = 0.01
 
 //How many iterations should we run before we are certain of an escape velocity?
-const val ESCAPE_VELOCITY_TEST_ITERATIONS: Int = 100
+const val ESCAPE_VELOCITY_TEST_ITERATIONS: Int = 50
 
 data class Color(val r: Double, val g: Double, val b: Double)
 
-
-
-
-data class ComplexNumber(var real: Double, var imag: Double) {
+data class ComplexNumber(val real: Double, val imag: Double) {
     operator fun minus(c: ComplexNumber): ComplexNumber { return ComplexNumber(real-c.real, imag-c.imag)}
     operator fun plus(c: ComplexNumber): ComplexNumber { return ComplexNumber(real+c.real, imag+c.imag)}
     operator fun times(c: ComplexNumber): ComplexNumber {
@@ -58,36 +46,115 @@ data class ComplexNumber(var real: Double, var imag: Double) {
     }
 }
 
-fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HEIGHT) {
-    if ( !glfwInit()) {
-        throw Exception("Failed to initialize GLFW.")
-    }
-    glfwDefaultWindowHints()
-    //Do not allow resize
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
-    glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE)
-    window = glfwCreateWindow(windowSizeW, windowSizeH, "Mandelbrot Set", 0, 0)
-    if (window == MemoryUtil.NULL) {
-        throw Exception("Failed to initialize window.")
-    }
-    glfwMakeContextCurrent(window)
 
-    //Key callbacks come later
-    //glfwSetKeyCallback(window, object_with_keycallback_funcs::glfwKeypressCallbackFunc)
+class mandelbrotView(private val window: Long) {
+    init {
+        glfwSetKeyCallback(window, this::glfwKeypressCallback)
+    }
 
-    // GL configuration comes AFTER we make the window our current context, otherwise errors
-    GL.createCapabilities()
-    GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f) //white background
-    GL11.glOrtho(-2.0, 1.0, -1.0, 1.0, -1.0, 1.0)
-    GL11.glViewport(0, 0, WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT)
-    glfwShowWindow(window)
+    private var currentZoomLevel: Double = 1.0 //zoomed out at 100%
+    private var startHeight: Double = 2.0
+    //Coords defined by top-left corner
+    private var BOUND_TOP: Double = 1.0
+    private var BOUND_BOTTOM: Double = -1.0
+    private var BOUND_LEFT: Double = -2.0
+    private var BOUND_RIGHT: Double = 1.0
+
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun glfwKeypressCallback(window: Long, key: Int, scancode: Int, action: Int, mods: Int) {
+        if (action == GLFW_PRESS || action == GLFW_REPEAT)
+            when (key) {
+                GLFW_KEY_UP -> {
+                    BOUND_TOP += BOUND_TOP*PAN_PCT_INCREMENT
+                }
+                GLFW_KEY_DOWN -> {
+                    BOUND_TOP -= BOUND_TOP*PAN_PCT_INCREMENT
+                }
+                GLFW_KEY_LEFT -> {
+                    BOUND_LEFT -= BOUND_TOP*PAN_PCT_INCREMENT
+                }
+                GLFW_KEY_RIGHT -> {
+                    BOUND_LEFT += BOUND_TOP*PAN_PCT_INCREMENT
+                }
+                GLFW_KEY_KP_ADD -> currentZoomLevel += ZOOM_INCREMENT
+                GLFW_KEY_KP_SUBTRACT -> currentZoomLevel -= ZOOM_INCREMENT
+                GLFW_KEY_KP_0 -> resetAll()
+            }
+        updateView()
+    }
+
+    private fun resetAll() {
+        BOUND_TOP = 1.0
+        BOUND_LEFT = -2.0
+        currentZoomLevel = 1.0
+        updateView()
+    }
+
+    private fun updateView() {
+        //This function is called whenever we pan or zoom. When we zoom we dont change the top-left
+        //bounds so no need to update them. When we pan we have already changed those coordinates
+        //so the only thing left to update in any case are the bottom and right bounds.
+        val height: Double = currentZoomLevel * startHeight
+        BOUND_BOTTOM = BOUND_TOP - height
+        BOUND_RIGHT = BOUND_LEFT + (height * (3.0/2.0))
+        /*
+        ABSOLUTELY CRITICAL CALL! I was pulling my hair out over how to get pan/zoom working and
+        nothing was working correctly until I added this single call. Without this, calls to glOrtho
+        just modify the current projection matrix. By calling glLoadIdentity(), we load the identity
+        matrix first, essentially resetting us to the default state, before we reset our ortho matrix.
+        */
+        glLoadIdentity()
+        GL11.glOrtho(BOUND_LEFT, BOUND_RIGHT, BOUND_BOTTOM, BOUND_TOP, -1.0, 1.0)
+        redrawView()
+    }
+
+    private fun mandelbrotsimple(): Map<ComplexNumber, Color> {
+        //will clean this up later
+        //real and imaginary parts, x and y respectively.
+        //Starting in the top left (-2, 1)
+        var curImagCoord: Double = BOUND_TOP
+        val cordlist = mutableMapOf<ComplexNumber, Color>()
+        //Scanning left to right then top to bottom
+        //outer loop, top to bottom, Imaginary coord, y
+        while (curImagCoord in BOUND_BOTTOM..BOUND_TOP) {
+            var real: Double = BOUND_LEFT
+            while (real in BOUND_LEFT..BOUND_RIGHT) {
+                //doing stuffs, mandelbrot-ey stuff
+                val escapevelocity: Double = findEscapeVelocity(ComplexNumber(real, curImagCoord))
+                if (escapevelocity == 0.0) {
+                    cordlist[ComplexNumber(real, curImagCoord)] = Color(0.0,0.0,0.0) //Black color, inside the set
+                }
+                else {
+                    cordlist[ComplexNumber(real, curImagCoord)] = Color(escapevelocity/ESCAPE_VELOCITY_TEST_ITERATIONS,cos(escapevelocity),sin(escapevelocity/ESCAPE_VELOCITY_TEST_ITERATIONS))
+                }
+                real += RESOLUTION_LIMIT_X
+            }
+            curImagCoord -= RESOLUTION_LIMIT_Y
+        }
+        println("Finished iterating over the Imaginary axis.")
+        return cordlist
+    }
+
+    fun redrawView() {
+        glPointSize(6.0f)
+        glClear(GL_COLOR_BUFFER_BIT)
+        glBegin(GL_POINTS)
+        val s = mandelbrotsimple()
+        for ((cord, color) in s) {
+            glColor3d(color.r, color.g, color.b)
+            glVertex2d(cord.real, cord.imag)
+        }
+        glEnd()
+        glfwSwapBuffers(window)
+    }
 }
 
 //Check if a complex number is bounded or escapes.
-//If it escapes (is greater than 2) it returns the velocity.
+//If it escapes (is greater than 2) it returns the the current iteration number.
 //If its bounded it returns 0
 fun findEscapeVelocity(c: ComplexNumber): Double {
-    var z: ComplexNumber = ComplexNumber(0.0, 0.0)
+    var z = ComplexNumber(0.0, 0.0)
     for (iter in 1..ESCAPE_VELOCITY_TEST_ITERATIONS) {
         if (z.magnitude() > 2.0) {
             return iter.toDouble()
@@ -97,53 +164,36 @@ fun findEscapeVelocity(c: ComplexNumber): Double {
     return 0.0
 }
 
-fun mandelbrotsimple(): Map<ComplexNumber, Color> {
-    //will clean this up later
-
-    //real and imaginary parts, x and y respectively.
-    //Starting in the top left (-2, 1)
-    var curImagCoord: Double = 1.0
-
-    val cordlist = mutableMapOf<ComplexNumber, Color>()
-    //Scanning left to right then top to bottom
-    //outer loop, top to bottom, Imaginary coord, y
-    while (curImagCoord in -CURRENT_Y_LIM..CURRENT_Y_LIM) {
-        var real: Double = -2.0
-        while (real in -2.0..1.0) {
-            //doing stuffs, mandelbrot-ey stuff
-            val escapevelocity: Double = findEscapeVelocity(ComplexNumber(real, curImagCoord))
-            if (escapevelocity == 0.0) {
-                cordlist[ComplexNumber(real, curImagCoord)] = Color(0.0,0.0,0.0) //Black color, inside the set
-            } //in the set, bounded value
-            else {
-                cordlist[ComplexNumber(real, curImagCoord)] = Color(escapevelocity/ESCAPE_VELOCITY_TEST_ITERATIONS,cos(escapevelocity),sin(escapevelocity/ESCAPE_VELOCITY_TEST_ITERATIONS)) //Black color, inside the set
-            }
-            real += RESOLUTION_LIMIT_X
-        }
-        curImagCoord -= RESOLUTION_LIMIT_Y
-    }
-    println("Finished iterating over the Imaginary axis.")
-    println("Mandelbrot tiem?")
-    return cordlist
-}
-
 fun main(args: Array<String>) {
     init()
-    println("Generating simple Mandelbrot set (not coloring)")
-    val setcoords = mandelbrotsimple()
-    println("Done generating set, printing to window...")
+    println("Generating simple Mandelbrot set (this could take a while)...")
+    val viewControl = mandelbrotView(window)
+    viewControl.redrawView()
+    //and wait for any keyboard stuffs now
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents()
-        glPointSize(5.0f)
-        glClear(GL_COLOR_BUFFER_BIT)
-        glBegin(GL_POINTS)
-        for ((cord, color) in setcoords) {
-            glColor3d(color.r, color.g, color.b)
-            glVertex2d(cord.real, cord.imag)
-        }
-        glEnd()
-        glfwSwapBuffers(window)
-        //glFlush()
-        Thread.sleep(100) //only paint every 100ms
+        Thread.sleep(50) //only paint every 100ms
     }
+}
+
+fun init(windowSizeW: Int = WINDOW_SIZE_WIDTH, windowSizeH: Int = WINDOW_SIZE_HEIGHT) {
+    if ( !glfwInit()) {
+        throw Exception("Failed to initialize GLFW.")
+    }
+    glfwDefaultWindowHints()
+    //Do not allow resize
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE)
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE)
+    window = glfwCreateWindow(windowSizeW, windowSizeH, "Mandelbrot Set", 0, 0)
+    if (window == MemoryUtil.NULL) {
+        throw Exception("Failed to initialize window.")
+    }
+    glfwMakeContextCurrent(window)
+    // GL configuration comes AFTER we make the window our current context, otherwise errors
+    GL.createCapabilities()
+    GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f) //white background
+    GL11.glViewport(0, 0, WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT)
+    GL11.glMatrixMode(GL_PROJECTION)
+    GL11.glOrtho(-2.0, 1.0, -1.0, 1.0, -1.0, 1.0)
+    glfwShowWindow(window)
 }
